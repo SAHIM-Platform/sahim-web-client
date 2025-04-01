3b8fdf8a12e0925ac7f71461f1587213e1d7e486
Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>
2025-04-01 10:58:48 +0300

    feat(ui): implement app info sidebar component, closes #13

--- src/components/app/AppLayoutContent.tsx
+++ src/components/app/AppLayoutContent.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import AppNavbar from "./AppNavbar/AppNavbar";
 import { useSearchShortcuts } from "@/hooks/useSearchShortcuts";
 import AppMenuSidebar from "./AppMenuSidebar";
+import AppInfoSidebar from "./AppInfoSidebar";
 
 export default function AppLayoutContent({
   children,
@@ -22,6 +23,8 @@ export default function AppLayoutContent({
       />
 
       <AppMenuSidebar />
+
+      <AppInfoSidebar />
     </div>
   );
 } 
