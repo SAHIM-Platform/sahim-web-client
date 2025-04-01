40431a07bc7a8b1efe714cd922d7fc8ded686e02
Mohannad-Almageedy <mohannadalmageedy73@gmail.com>
2025-04-01 11:30:30 +0300

    refactor: redirect homepage to /login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
-import Container from '@/components/Container';
-import LoginForm from '@/components/LoginForm';
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <main className="bg-[url('/login-bg.jpg')] bg-no-repeat bg-fixed bg-cover bg-center min-h-screen overflow-x-hidden">
-      <Container>
-        <div className='flex items-center justify-center min-h-screen py-6 sm:py-8 lg:py-10 px-4 sm:px-6 lg:px-8'>
-          <LoginForm />
-        </div>
-      </Container>
-    </main>
-  );
+export default function HomePage() {
+  redirect("/login");
 }
-
