10797d7e77f17f151accb3213bd3f920ad82cef9
Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>
2025-04-05 06:18:05 +0300

    refactor(auth): enhance useLogout hook with error handling and loading state

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -4,27 +4,43 @@ import { logoutService } from "@/services/auth/logoutService";
 import useAuth from "./useAuth";
 import { useRouter } from "next/navigation";
 import useAxios from "./useAxios";
+import { useState } from "react";
+import ERROR_MESSAGES from "@/utils/api/ERROR_MESSAGES";
 
 const useLogout = () => {
   const { setAuth } = useAuth();
   const axiosPrivate = useAxios();
   const router = useRouter();
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const logout = async () => {
+    setError(null);
+    setIsLoading(true);
+
     try {
-      await logoutService(axiosPrivate);
-    } catch (error) {
-      console.error("Logout failed:", error);
-    } finally {
+      const response = await logoutService(axiosPrivate);
+
+      if (!response.success) {
+        setError(response.error?.message || ERROR_MESSAGES.logout.DEFAULT);
+        return;
+      }
+
       setAuth({
         accessToken: undefined,
         loading: false,
       });
-      router.push("/");
+      router.push("/login");
+
+    } catch (error) {
+      console.error("Logout failed:", error);
+      setError(ERROR_MESSAGES.logout.DEFAULT);
+    } finally {
+      setIsLoading(false);
     }
   };
 
-  return logout;
+  return { logout, error, isLoading };
 };
 
 export default useLogout;
