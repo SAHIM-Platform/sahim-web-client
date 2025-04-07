720d428a70b02b4e8aaebb1d5dc51c324392b9a0
Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>
2025-04-07 23:45:16 +0300

    feat: complete implementation of useAuthRedirect
    - linked useAuthRedirect with auth system in exploe and login routes
    - format useAuthRedirect file

--- src/app/(app)/explore/page.tsx
+++ src/app/(app)/explore/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import ThreadListing from "@/components/app/ThreadListing/ThreadListing";
+import useAuth from "@/hooks/useAuth";
 import useAuthRedirect from "@/hooks/UseRedirect";
 
 export default function ExplorePage() {
-useAuthRedirect(() => false);
+  const { isAuthenticated } = useAuth();
+  useAuthRedirect(isAuthenticated);
 
   return (
     <ThreadListing />
--- src/hooks/UseRedirect.ts
+++ src/hooks/UseRedirect.ts
@@ -3,17 +3,16 @@
 import { useEffect } from "react";
 import { usePathname, useRouter } from "next/navigation";
 
-const useAuthRedirect = (isAuthenticated: () => boolean): void => {
+const useAuthRedirect = (isAuthenticated: boolean): void => {
   const router = useRouter();
   const pathName = usePathname();
 
   useEffect(() => {
-    if (isAuthenticated()) {
+    if (isAuthenticated) {
       if (pathName === "/login" || pathName === "/signup")
         router.push("/explore");
     } else {
-      if (
-        pathName.startsWith("/explore") || pathName.startsWith("/discussion")) {
+      if (pathName.startsWith("/explore") || pathName.startsWith("/discussion")) {
         router.push("/login");
       }
     }
