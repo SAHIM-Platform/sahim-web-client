3be3b382955f28f00a4dc5e61433cfd052264bf1
Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>
2025-04-11 09:23:02 +0300

    feat: create categories page in `/categories/`
    - created CategoriesListing component and reused it in categories page and AppInfoSidebar

---
+++ src/app/(app)/categories/page.tsx
@@ -0,0 +1,17 @@
+import CategoriesListing from "@/components/app/CategoriesListing";
+
+export default function CategoriesPage() {
+  return (
+    <div className="flex flex-col gap-4">
+      <div>
+        <h1 className="text-lg sm:text-xl lg:text-2xl font-semibold">
+          جميع التصنيفات
+        </h1>
+        <p className="mt-2 text-xs sm:text-sm lg:text-base text-gray-500">
+          تصفّح جميع تصنيفات المناقشات المتاحة
+        </p>
+      </div>
+      <CategoriesListing />
+    </div>
+  );
+}
