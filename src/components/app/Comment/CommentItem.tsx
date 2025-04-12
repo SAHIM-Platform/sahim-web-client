e439d69b2690ad933a2bb1456f7bba167d2fde92
Ayman-Mokhtar535 <126535129+Ayman-Mokhtar535@users.noreply.github.com>
2025-04-12 08:06:35 +0300

    feat(134): render comments in markdown preview mode (#148)
    Co-authored-by: Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>

--- src/components/app/Comment/CommentItem.tsx
+++ src/components/app/Comment/CommentItem.tsx
@@ -5,6 +5,8 @@ import Button from "@/components/Button";
 import { useRef, useState, useEffect } from "react";
 import Divider from "@/components/Divider";
 import Textarea from "@/components/Textarea";
+import remarkGfm from "remark-gfm";
+import ReactMarkdown from "react-markdown";
 import useAuth from "@/hooks/useAuth";
 import toast from "react-hot-toast";
 import ERROR_MESSAGES from "@/utils/api/ERROR_MESSAGES";
@@ -216,9 +218,53 @@ function CommentItem({
             </div>
           </div>
         ) : (
-          <p className="mt-4 text-xs text-gray-600 leading-relaxed">
-            {content}
-          </p>
+          <div className="mt-4 text-xs text-gray-600 leading-relaxed">
+          <ReactMarkdown 
+          remarkPlugins={[remarkGfm]}
+          children={content}
+          components={{
+            a: ({ href, children }) => (
+              <a
+                href={href}
+                className='text-blue-400 hover:text-blue-300 underline'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                {children}
+              </a>
+            ),
+            h1: ({ children }) => (
+              <p className='text-2xl font-bold mb-4'>{children}</p>
+            ),
+            h2: ({ children }) => (
+              <p className='text-xl font-bold mb-4 mt-4'>{children}</p>
+            ),
+            h3: ({ children }) => (
+              <p className='text-lg font-semibold mb-2 mt-2'>{children}</p>
+            ),
+            ul: ({ children }) => (
+              <ul className='list-disc pl-2 mb-4 space-y-1'>{children}</ul>
+            ),
+            ol: ({ children }) => (
+              <ol className='list-decimal pl-6 mb-4 space-y-1'>{children}</ol>
+            ),
+            p: ({ children }) => (
+              <p className='leading-8 mb-2 font-light'>{children}</p>
+            ),
+            li: ({ children }) => (
+              <li className='leading-7 pl-2 marker:text-gray-400 [&>strong]:mt-0 [&>strong]:inline'>{children}</li>
+            ),
+            strong: ({ children }) => (
+              <strong className='font-semibold '>{children}</strong>
+            ),
+            code: ({ children }) => (
+              <div className="bg-gray-100 text-xs p-2 rounded-lg font-semibold" dir="ltr">
+              <code>{children}</code>
+              </div>
+            ),
+          }}
+          />
+          </div>
         )}
 
         <div className="mt-4">
