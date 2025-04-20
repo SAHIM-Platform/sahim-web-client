91fe752d28be127ae53082946952d237b58bea1b
Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>
2025-04-20 13:45:14 +0300

    feat: add limit for chars in CategoryBadge

--- src/components/app/Badge/CategoryBadge.tsx
+++ src/components/app/Badge/CategoryBadge.tsx
@@ -1,11 +1,15 @@
 export interface CategoryBadgeProps {
   name: string
+  maxChars?: number;
 }
 
-function CategoryBadge({name}: CategoryBadgeProps) {
+function CategoryBadge({ name, maxChars = 15 }: CategoryBadgeProps) {
   return (
     <span className="px-1.5 py-0.5 rounded-full bg-primary/5 text-primary text-[12px] font-medium border border-primary/10">
-      {name}
+
+      {maxChars && name.length > maxChars
+        ? `${name.substring(0, maxChars)}...`
+        : name}
     </span>
   )
 }
