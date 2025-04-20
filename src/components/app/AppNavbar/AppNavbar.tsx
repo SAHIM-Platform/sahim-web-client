320844e2be27800ca33d75fbcb7e5cd85e38ef78
Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>
2025-04-20 13:53:28 +0300

    feat: styling imporvements

--- src/components/app/AppNavbar/AppNavbar.tsx
+++ src/components/app/AppNavbar/AppNavbar.tsx
@@ -23,7 +23,7 @@ function AppNavbar({
   return (
     <div className="fixed top-0 left-0 right-0 h-14 border-b-2 border z-50 bg-[#fafafa]">
       <div className="h-full mx-auto px-4 flex items-center justify-between">
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2 md:gap-4">
           <button
             onClick={onToggleMenuSidebar}
             className="lg:hidden p-2 hover:bg-gray-100 rounded-lg"
@@ -31,7 +31,7 @@ function AppNavbar({
             <Menu className="w-5 h-5 text-gray-700" />
           </button>
 
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-1 md:gap-2">
             <Logo widthSize="sm" />
             <Link href="/" className="text-xl font-bold text-secondary">ساهم</Link>
           </div>
@@ -42,7 +42,7 @@ function AppNavbar({
           />
         </div>
 
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2 md:gap-4">
           {/* <NotificationsBadge /> */}
 
           <UserDropdownMenu />
